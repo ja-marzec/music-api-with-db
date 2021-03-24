de1ffeb929be0316bf89d3b915296bdff54181fe
Hubert-Marzecki <hubert.marzecki@gmail.com>
2021-03-23 21:40:39 -0700

    front adjustments

--- app-front/src/App.js
+++ app-front/src/App.js
@@ -1,36 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { set } from 'mongoose';
-
+import PersistentDrawerRight from './components/Header'
 
 
 function App() {
 
   const [data, setData] = useState([]);
 
-  useEffect(async () => {
-    try{
-      const result = await axios.get("http://localhost:5000");
-      setData(result.data)
-    }catch(err) {
-      console.log(err);
+  useEffect(() => {
+    async function loadData() {
+      try {
+        const result = await axios.get("http://localhost:5000");
+        setData(result.data)
+      } catch (err) {
+        console.log(err);
+      }
     }
-    },[])
+    loadData();
+    console.log(data);
+  }, [])
 
-    useEffect(() => {
-      console.log(data);
-      },[data])
+
+  const genres = () => {
+      return  data.map(_ => {
+        return (
+          <div class="genre__card">
+            <p>da</p>
+          </div>
+        )
+      })
+
+  }
 
   return (
     <div className="App">
-        {data.map(item => {
-          return (
-            <div>{item.title}</div>
-          )
-        })}
+      <PersistentDrawerRight />
+      <div className="container">
+        <h1> New Songs </h1>
+
+        <div className="songs__container">
+          {data.map(item => {
+            return (
+              <div class="card">
+                <a href={item.url} className="tittle"> {item.title}</a>
+              </div>
+            )
+          })}
+        </div>
+
+        <h1> Pick genre </h1>
+
+        <div className="genre__container">
+           {genres()}
+        </div>
+      </div>
     </div>
+
   );
 }
 
